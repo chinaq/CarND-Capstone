df8e482cec2aceff8d28d30a69faeed6da2db79f
Ryan Jonesford <ryanjonesford@gmail.com>
2018-11-10 12:14:49 -0500

    constants added

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -2,18 +2,7 @@ import rospy
 from yaw_controller import YawController
 from pid import PID
 from lowpass import LowPassFilter
-
-GAS_DENSITY = 2.858
-ONE_MPH = 0.44704
-CLOSE_TO_ZERO_SPEED = 0.15 # Consider this 0 speed
-THROTTLE_MIN = 0
-THROTTLE_MAX = 1
-
-THROTTLE_KP = 1000
-THROTTLE_KI = 0.001
-THROTTLE_KD = 50.0
-
-BRAKE_STATIONARY_FORCE = 700 # Nm to keep car stationary
+import constants as const
 
 class Controller(object):
     def __init__(self,
@@ -30,7 +19,7 @@ class Controller(object):
         self.decel_limit = decel_limit
         self.vehicle_mass = vehicle_mass
         self.wheel_radius = wheel_radius
-        min_speed = CLOSE_TO_ZERO_SPEED
+        min_speed = const.CLOSE_TO_ZERO_SPEED
 
         self.yaw_controller = YawController(wheel_base,
                                             steer_ratio,
@@ -38,14 +27,14 @@ class Controller(object):
                                             max_lat_accel,
                                             max_steer_angle)
 
-        self.throttle_kp = THROTTLE_KP
-        self.throttle_ki = THROTTLE_KI
-        self.throttle_kd = THROTTLE_KD
+        self.throttle_kp = const.THROTTLE_KP
+        self.throttle_ki = const.THROTTLE_KI
+        self.throttle_kd = const.THROTTLE_KD
         self.throttle_controller = PID(self.throttle_kp,
                                        self.throttle_ki,
                                        self.throttle_kd,
-                                       mn=THROTTLE_MIN,
-                                       mx=THROTTLE_MAX)
+                                       mn=const.THROTTLE_MIN,
+                                       mx=const.THROTTLE_MAX)
         
         self.vel_tau = 0.5 # cutoff freq
         self.vel_ts = 0.02 # ms
@@ -74,9 +63,9 @@ class Controller(object):
 
         throttle = self.throttle_controller.step(vel_error,dt)
 
-        if linear_vel == 0 and current_vel < CLOSE_TO_ZERO_SPEED:
+        if linear_vel == 0 and current_vel < const.CLOSE_TO_ZERO_SPEED:
             throttle = 0
-            brake = BRAKE_STATIONARY_FORCE 
+            brake = const.BRAKE_STATIONARY_FORCE 
         elif throttle < .1 and vel_error < 0:
             throttle = 0
             brake = abs( max(vel_error, self.decel_limit) * self.vehicle_mass * self.wheel_radius)
--- ros/src/twist_controller/yaw_controller.py
+++ ros/src/twist_controller/yaw_controller.py
@@ -1,5 +1,5 @@
 from math import atan
-from twist_controller import CLOSE_TO_ZERO_SPEED
+import constants as const
 
 class YawController(object):
     def __init__(self, wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle):
@@ -19,7 +19,7 @@ class YawController(object):
     def get_steering(self, linear_velocity, angular_velocity, current_velocity):
         angular_velocity = current_velocity * angular_velocity / linear_velocity if abs(linear_velocity) > 0. else 0.
 
-        if abs(current_velocity) > CLOSE_TO_ZERO_SPEED:
+        if abs(current_velocity) > const.CLOSE_TO_ZERO_SPEED:
             max_yaw_rate = abs(self.max_lat_accel / current_velocity)
             angular_velocity = max(-max_yaw_rate, min(max_yaw_rate, angular_velocity))
 
