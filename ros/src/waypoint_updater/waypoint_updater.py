9dd01d51fd02edd3f069ecc2a4850c207b510849
Ryan Jonesford <ryanjonesford@gmail.com>
2018-11-13 22:00:21 -0500

    Update ros/src/waypoint_updater/waypoint_updater.py

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -78,7 +78,7 @@ class WaypointUpdater(object):
                 temp_wp.pose = wp.pose
                 if stop_idx >= STOPLINE:
                     dist = self.distance(base_wpts, i, stop_idx)
-                    vel = math.log(DECEL_RATE^2*dist)
+                    vel = math.log(DECEL_RATE**2*dist)
                     if vel < 1.:
                         vel = 0.
                 else:
