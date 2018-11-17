9307b9bb6a2551594c22f41e756e1d43310c8d17
Ryan Jonesford <ryanjonesford@gmail.com>
2018-11-16 21:55:03 -0500

    Update waypoint_updater.py

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -25,7 +25,7 @@ as well as to verify your TL classifier.
 LOOKAHEAD_WPS = 50 # Number of waypoints we will publish. You can change this number
 UPDATE_RATE = 30 #hz
 NO_WP = -1
-DECEL_RATE = 4.9 # m/s^2
+DECEL_RATE = 1.5 # m/s^2
 STOPLINE = 3 # waypoints behind stopline to stop
 DELAY = 20. # update difference between this node and twist_controller in hz
 
@@ -94,7 +94,7 @@ class WaypointUpdater(object):
                     dist = self.distance(base_wpts, i, stop_idx)
                     # account for system lag
                     delay_s = 1./DELAY
-                    dist = dist + self.get_waypoint_velocity(base_wpts[i])*delay_s+.5*DECEL_RATE*delay_s*delay_s
+                    dist += self.get_waypoint_velocity(base_wpts[i])*delay_s+.5*DECEL_RATE*delay_s*delay_s
                     # v^2 = vo^2 + 2*a*(x-xo)
                     # v^2 = 0 + 2*a*(dist)
                     # v = sqrt(2*a*dist)
